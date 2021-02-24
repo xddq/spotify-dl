c3b7bb6214780d8d175f9851f2c192adc3729629
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-02-24 20:36:10 +0100

    Adds error check if we have a bad playlist id or bad api token. Adapts comments

--- spotify-dl.sh
+++ spotify-dl.sh
@@ -3,15 +3,15 @@
 # Author: Pierre Dahmani
 # Created: 19.02.2021
 #
-# Description: Small shell script used to download your own spotify playlists.
+# Description: Small shell script used to download your own
+# (or any publicly available) spotify playlists.
 # Uses the matching tracks on youtube and the youtube-dl tool.
-# Tested on Ubuntu 18.04.5 LTS.
 
 
 # CHANGE THESE TWO VALUES
 # id of your created api token
 API_TOKEN=BQDZTkCAAiM0P_kL819Ah4RU1I6qm4hcdKKmq41twav2ldfKHYPnZ1wXK8YyzkfU_Z86uONgczEkebZ0gMxDYTef_Dlf4wlWBWGj5iq4RC-ObHmX5xHwJ57LpI3s4wJZsnsW4Fc7SA
-# id of your playlist you want to download
+# id of the playlist you want to download
 PLAYLIST_ID=4oMRIwefQWsVxLRLqECDep
 
 
@@ -21,11 +21,19 @@ curl -X "GET" "https://api.spotify.com/v1/playlists/$PLAYLIST_ID/tracks?fields=i
     -H "Authorization: Bearer $API_TOKEN" | grep name > spotify-playlist
 
 
-# Loops through each track and calls youtube-dl.
+# checks if api token is invalid or playlist does not exist by counting the
+# lines of the file we get by calling the api.
+if [ -z $(cat spotify-playlist | wc -l) ]
+then
+    echo "Either the api token is wrong or the playlist does not exist."
+    exit 1
+fi
+
+
+# loops through each track and calls youtube-dl.
 # Iterations will be lines /2 because we have name one line and album
-# one line
+# one line. Increase counter by 2 per iteration rather than dividing by 2.
 TRACK_COUNT=$(cat spotify-playlist | wc -l)
-# Increase counter by 2 per iteration rather than dividing by 2.
 COUNTER=0
 while [ $COUNTER -lt $TRACK_COUNT ]
 do
@@ -49,13 +57,16 @@ do
     TRACK_NAME=$(cat spotify-playlist | awk "NR==`expr $COUNTER + 1`" | \
         awk -F '"name" : ' '{print $2}')
 
-    # Calls youtube-dl to download the first file that matches our search
-    # query. Will be stored in the ./downloads directory.
+    # calls youtube-dl to download the first file that matches our search
+    # query. Results will be stored in the ./downloads directory.
     youtube-dl -o "./downloads/%(title)s.%(ext)s" --extract-audio \
         ytsearch:"$ARTIST_NAME $TRACK_NAME music"
 
     COUNTER=`expr $COUNTER + 2`
 done
 
-# clean up the temporary file
+# cleans up the temporary file
 rm spotify-playlist
+
+# notify user that we are done
+echo "Done downloading your playlist. Check results in the ./downloads folder."
