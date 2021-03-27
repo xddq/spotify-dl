93a873e7b00d7f50264bc3872ec37b720f80c313
Pierre Dahmani <Pierre.dahmani@alumni.fh-aachen.de>
2021-03-27 22:52:54 +0100

    prepends track number that matching your playlist order to each track. creates folder with your playlist name when downloading. adds option to download as mp3. other default is .opus

--- spotify-dl.sh
+++ spotify-dl.sh
@@ -10,63 +10,91 @@
 
 # CHANGE THESE TWO VALUES
 # id of your created api token
-API_TOKEN=BQDZTkCAAiM0P_kL819Ah4RU1I6qm4hcdKKmq41twav2ldfKHYPnZ1wXK8YyzkfU_Z86uONgczEkebZ0gMxDYTef_Dlf4wlWBWGj5iq4RC-ObHmX5xHwJ57LpI3s4wJZsnsW4Fc7SA
+API_TOKEN=BQBsP6Ok3CZcRKBPIgcdUnK6pETmCA-6Y718v1otNh3ERCyDGKYNZNZrUVDdNrHCQ8d5kapRCvSxG7q8byFxijY53gDeUSi4tfsCIbzJpKm9mP_w8sJvWjEmzRThMTaK23SRORBaNw
 # id of the playlist you want to download
-PLAYLIST_ID=4oMRIwefQWsVxLRLqECDep
+PLAYLIST_ID=2Qt6vSalV7iAwsIlmtM14L
+# If you set this to TRUE you will end up with .mp3 files. If you set this to
+# any other value you will end up with .opus files.
+STORE_AS_MP3=TRUE
+
+
+# sets argument for youtube-dl by given value of $STORE_AS_MP3
+if [ $STORE_AS_MP3 = 'TRUE' ]
+then
+   STORE_AS_MP3="--audio-format mp3"
+else
+   STORE_AS_MP3=""
+fi
 
 
 # gets artist and track names and stores them in a file
 curl -X "GET" "https://api.spotify.com/v1/playlists/$PLAYLIST_ID/tracks?fields=items(track(name,artists(name)))" \
     -H "Accept: application/json" -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $API_TOKEN" | grep name > spotify-playlist
+    -H "Authorization: Bearer $API_TOKEN" | grep name > get-playlist-tracks
 
 
 # checks if api token is invalid or playlist does not exist by counting the
 # lines of the file we get by calling the api.
-if [ -z $(cat spotify-playlist | wc -l) ]
+if [ -z $(cat get-playlist-tracks | wc -l) ]
 then
     echo "Either the api token is wrong or the playlist does not exist."
     exit 1
 fi
 
+# queries the playlist name and stores it inside PLAYLIST_NAME
+curl -X "GET" \
+    "https://api.spotify.com/v1/playlists/$PLAYLIST_ID/?fields=name" \
+    -H "Accept: application/json" -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $API_TOKEN"  > get-playlist-name
+PLAYLIST_NAME=$(cat get-playlist-name | grep name | head -n 1 \
+    | awk -F \" '{print $4}')
 
 # loops through each track and calls youtube-dl.
 # Iterations will be lines /2 because we have name one line and album
 # one line. Increase counter by 2 per iteration rather than dividing by 2.
-TRACK_COUNT=$(cat spotify-playlist | wc -l)
+TRACK_COUNT=$(cat get-playlist-tracks | wc -l)
 COUNTER=1
+TRACK_NUMBER=1
 while [ $COUNTER -lt $TRACK_COUNT ]
 do
     # Uses '"name" : ' as delim to get the artist name.
-    ARTIST_NAME=$(cat spotify-playlist | awk "NR==$COUNTER" | \
+    ARTIST_NAME=$(cat get-playlist-tracks | awk "NR==$COUNTER" | \
         awk -F '"name" : ' '{print $2}')
 
     # MAYBE(pierre): Could just concat the artists if IS_ARTIST.
     # Artist entries have 8 spaces per row. Track entries have less.
     # If we have multiple artists for a track we increase the counter until the
     # next entry is NOT an artist. Then we continue.
-    IS_ARTIST=$(cat spotify-playlist | awk "NR==`expr $COUNTER + 1`" \
+    IS_ARTIST=$(cat get-playlist-tracks | awk "NR==`expr $COUNTER + 1`" \
         | egrep "[ ]{8}")
     while [ $? -eq 0 ]
     do
         COUNTER=`expr $COUNTER + 1`
-        IS_ARTIST=$(cat spotify-playlist | awk "NR==`expr $COUNTER + 1`" \
+        IS_ARTIST=$(cat get-playlist-tracks | awk "NR==`expr $COUNTER + 1`" \
             | egrep "[ ]{8}")
     done
 
-    TRACK_NAME=$(cat spotify-playlist | awk "NR==`expr $COUNTER + 1`" | \
+    TRACK_NAME=$(cat get-playlist-tracks | awk "NR==`expr $COUNTER + 1`" | \
         awk -F '"name" : ' '{print $2}')
 
-    # calls youtube-dl to download the first file that matches our search
-    # query. Results will be stored in the ./downloads directory.
-    youtube-dl -o "./downloads/%(title)s.%(ext)s" --extract-audio \
-        ytsearch:"$ARTIST_NAME $TRACK_NAME music"
+    # makes sure we will keep order as it is in our playlist.
+    # first track will be 001. tenth will be 010 etc..
+    TRACK_NUMBER_THREE_POSITIONS=$(printf "%03d" $TRACK_NUMBER)
 
+    # calls youtube-dl to download the first file that matches our search
+    # query. Results will be stored in the ./downloads/nameOfYourPlaylist
+    # directory.
+    youtube-dl --extract-audio $STORE_AS_MP3 \
+        -o "./downloads/$PLAYLIST_NAME/$TRACK_NUMBER_THREE_POSITIONS-%(title)s.%(ext)s" \
+        ytsearch:"$ARTIST_NAME $TRACK_NAME music official"
+
+    TRACK_NUMBER=`expr $TRACK_NUMBER + 1`
     COUNTER=`expr $COUNTER + 2`
 done
 
 # cleans up the temporary file
-rm spotify-playlist
+rm get-playlist-tracks get-playlist-name
 
-# notify user that we are done
-echo "Done downloading your playlist. Check results in the ./downloads folder."
+# notifies user that we are done
+echo "Done downloading your playlist. Check results in the
+./downloads/$PLAYLIST_NAME folder."
